d079cae30b8607359d7f67f4aca3503917f9cf69
as_star@qq.com <as_star@qq.com>
2018-02-28 23:18:18 +0800

    backbone

--- src/unit/Select.php
+++ src/unit/Select.php
@@ -6,48 +6,28 @@ namespace Baiz\Unit;
 class Select extends Element{
 	
 	function __construct($left,$right){
-		
 		$this->left = $left;
-		
 		$this->right = $right;
-		
 	}
 	
 	function match($string,$start=0){
-		
 		try{
-			
 			$left = $this->left->match($string,$start);
-			
 			if(empty($this->_name)){
-				
 				return $left;
-				
 			}
 			else{
-				
 				return [new \Baiz\Result\Result($this->_name,$string,$start,$left[sizeof($left)-1]->end)];
-				
 			}
-			
 		}
 		catch(Exception $e){
-			
 			$right = $this->right->match($string,$left[sizeof($left)-1]->end);
-			
 			if(empty($this->_name)){
-				
 				return $right;
-				
 			}
 			else{
-				
 				return [new \Baiz\Result\Result($this->_name,$string,$start,$right[sizeof($right)-1]->end)];
-				
 			}
-			
 		}
-		
 	}
-	
 }
