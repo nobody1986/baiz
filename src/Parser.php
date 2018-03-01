16d1ae796b635cbb2edac7a58406d508fa910d7d
as_star@qq.com <as_star@qq.com>
2018-03-01 23:40:48 +0800

    backbone

--- src/Parser.php
+++ src/Parser.php
@@ -3,6 +3,11 @@ namespace Baiz;
 
 class Parser{
     protected $_string;
+    /**
+     * t= x|y
+     * x="a"| "b"
+     * y = "c" | "d"
+     */
     public function parse($string){
         $this->_string = $string;
         
